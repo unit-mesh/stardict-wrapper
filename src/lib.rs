407214bda1733130f0a056b803729ab6b06fbec5
Phodal Huang <h@phodal.com>
2023-08-11 08:31:28 +0800

    fix; fix trim

--- src/lib.rs
+++ src/lib.rs
@@ -15,28 +15,26 @@ impl WrapperDict {
 
     pub fn translate(&mut self, word: &str) -> Option<String> {
         if let Ok(result) = self.std_dict.lookup(word) {
-            match result {
-                None => {
+            if let None = result {
+                return None;
+            } else if let Some(defs) = result {
+                if defs.len() == 0 {
                     return None;
                 }
-                Some(defs) => {
-                    if defs.len() == 0 {
-                        return return None;
-                    }
 
-                    let result = String::new();
-                    for def in &defs[0].segments {
-                        return if let Some(pos) = def.text.find(';') {
-                            let output = &def.text[..pos];
-                            Some(output.into())
-                        } else {
-                            let first_result = def.text.as_str();
-                            Some(first_result.into())
-                        }
+                let result = String::new();
+                for def in &defs[0].segments {
+                    return if let Some(pos) = def.text.find(';').or(def.text.find('\n')) {
+                        let output = &def.text[..pos];
+                        Some(output.into())
+                    } else {
+                        Some(def.text.clone())
                     }
-
-                    return Some(result);
                 }
+
+                return Some(result);
+            } else {
+                unreachable!()
             };
         } else {
             return None;
@@ -50,8 +48,8 @@ mod tests {
 
     #[test]
     fn test_translate() {
-        let mut dict = WrapperDict::init("/Users/phodal/Downloads/stardict-langdao-ce-gb-2.4.2");
-        let result = dict.translate("员工");
+        let mut dict = WrapperDict::init("/Users/phodal/Downloads/stardict-ncce-ce-2.4.2");
+        let result = dict.translate("对齐");
         assert_eq!(result, Some("hello".to_string()));
     }
 }
